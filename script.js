6af2e375d84e533e84573f205b3dc589143a4618
Junior Piovesan Silva <juniorpiovesan3022@outlook.com>
2023-05-18 17:07:35 -0300

    implementado função de pintar pixels e iniciar com uma cor e um board inicial

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-const colors = document.getElementsByClassName('color');
+const colors = document.querySelectorAll('.color');
 const pixelsContainer = document.getElementById('pixels__board');
 const inputVertical = document.getElementById('vertical');
 const inputHorizontal = document.getElementById('horizontal');
 const inputTamanho = document.getElementById('tamanho-pixel');
 const btn = document.getElementById('btn-submit');
+const colorSelected = document.querySelector('.color-selected');
+// const pixels = document.querySelectorAll('.pixel');
 
 // Adiciona informações no local storage.
 const addLocalStorage = (key, obj) => {
@@ -30,6 +32,28 @@ const getInformation = () => {
   return prefDate;
 };
 
+// Cria primeiro quadro e primeira cor selecionada
+const firstData = () => {
+  const board = getLocalStorage('infBoardPixels');
+  const color = getLocalStorage('selected-color');
+  if (!board) {
+    const prefDate = {
+      vertical: 20,
+      horizontal: 20,
+      tamanhoPixel: 20,
+    };
+
+    addLocalStorage('infBoardPixels', prefDate);
+  }
+  if (!color) {
+    const selectColor = {
+      selected: 'black',
+    };
+    colorSelected.style.backgroundColor = 'black';
+    addLocalStorage('selected-color', selectColor);
+  }
+};
+
 // Remove elementos filhos
 const removeElement = (elemento) => {
   while (elemento.firstChild) {
@@ -37,10 +61,13 @@ const removeElement = (elemento) => {
   }
 };
 
-// Cria quadro de pixels
+// Cria quadro de pixels, ao recarregar a pagina recuupera os dados
 const pixelsBoard = () => {
   removeElement(pixelsContainer);
   const data = getLocalStorage('infBoardPixels');
+  const color = getLocalStorage('selected-color');
+  colorSelected.style.backgroundColor = color.selected;
+
   const { vertical, horizontal, tamanhoPixel } = data;
   const tamanho = tamanhoPixel.toString();
 
@@ -55,31 +82,34 @@ const pixelsBoard = () => {
       pixel.style.height = `${tamanho}px`;
       pixel.style.width = `${tamanho}px`;
 
+      pixel.addEventListener('click', () => {
+        const { selected } = getLocalStorage('selected-color');
+        pixel.style.backgroundColor = selected;
+      });
+
       coluna.appendChild(pixel);
     }
     pixelsContainer.appendChild(coluna);
   }
 };
 
-// Cria quadro primário
-// const firstBoard = () => {
-//   const data = getLocalStorage();
-//   if (!data) {
-//     const firstData = {
-//       vertical: 20,
-//       horizontal: 60,
-//       tamanhoPixel: 20,
-//     };
-//     addLocalStorage('infBoardPixels', firstData);
-//   }
-//   pixelsBoard();
-// };
-
 btn.addEventListener('click', () => {
   getInformation();
   pixelsBoard();
 });
 
+// Seleciona cor da paleta de cores e salva no local storage
+colors.forEach((element) => {
+  element.addEventListener('click', () => {
+    const selectColor = {
+      selected: element.id,
+    };
+    colorSelected.style.backgroundColor = element.id;
+    addLocalStorage('selected-color', selectColor);
+  });
+});
+
 window.onload = () => {
+  firstData();
   pixelsBoard();
 };
